ccb4360cb409d65d42d00fab299ac87b6ed18aad
Patt0lino <investmentmoises@gmail.com>
2023-03-04 17:52:16 -0300

    adicionando rota de exclusao de posts

--- controllers/TopicsControllers.js
+++ controllers/TopicsControllers.js
@@ -1,5 +1,5 @@
 //models
-const {ContentModel, SectorsModel} = require('../models/AdminModel')
+const { ContentModel, SectorsModel } = require('../models/AdminModel')
 
 //helpers
 const checkUserForToken = require('../helpers/checkUserForToken')
@@ -7,118 +7,123 @@ const checkUserForToken = require('../helpers/checkUserForToken')
 //rotas para os Admins
 
 //cria tópico, salvar id e titulo no array de areas, dentro de Model de setores
-exports.create = async (req,res)=>{
+exports.create = async (req, res) => {
     const user = await checkUserForToken(req)
     //check user is admin
-    if(user.isAdmin !==true){
-       return res.status(401).json({message:"Area somente para usuários administradores!"})
+    if (user.isAdmin !== true) {
+        return res.status(401).json({ message: "Area somente para usuários administradores!" })
     }
 
-    const {area,sectorAcess, title, steps,obs} = req.body
+    const { area, sectorAcess, title, steps, obs } = req.body
 
     //tratando dados
     const areaCheck = area && area.trim().toUpperCase()
     const titleCheck = title && title.trim().toUpperCase()
     const sectorAcessCheck = sectorAcess && sectorAcess.trim().toUpperCase()
 
-    if(!areaCheck || !titleCheck || !steps){
-       return res.status(401).json({message:"Preencha os dados corretamente!"})
+    if (!areaCheck || !titleCheck || !steps) {
+        return res.status(401).json({ message: "Preencha os dados corretamente!" })
     }
-    
-    //checa se setor existe
-    try{
-        const checkSectorExist = await SectorsModel.findOne({admin:user._id, sector:sectorAcess})
 
-        if(checkSectorExist===null){
-            return res.status(401).json({message:"Setor não existe, crie o setor e depois um tópico nele!"})
+    //checa se setor existe
+    try {
+        const checkSectorExist = await SectorsModel.findOne({ admin: user._id, sector: sectorAcess })
+
+        if (checkSectorExist === null) {
+            return res.status(401).json({ message: "Setor não existe, crie o setor e depois um tópico nele!" })
         }
-    }catch(error){
-      return  res.status(401).json({message:"Erro ao checar se setor existe, tente mais tarde!"})
+    } catch (error) {
+        return res.status(401).json({ message: "Erro ao checar se setor existe, tente mais tarde!" })
     }
 
     //salva tópico no banco de dados
     try {
         const ContentSave = {
-            admin:user._id,
-            area:areaCheck,
-            sectorAcess:sectorAcessCheck,
-            title:titleCheck,
-            obs:obs,
-            steps:steps
+            admin: user._id,
+            area: areaCheck,
+            sectorAcess: sectorAcessCheck,
+            title: titleCheck,
+            obs: obs,
+            steps: steps
         }
-        
+
         const contentDB = await ContentModel.create(ContentSave)
         //adiciona titulo e id do tópico no array de sectors
-            const refAreaAndTopics = {
-                idPub:contentDB._id,
-                title:contentDB.title
-            }
+        const refAreaAndTopics = {
+            idPub: contentDB._id,
+            title: contentDB.title
+        }
 
 
         //checar se a area está cadastrada no banco
-            //busca todos os setores do banco, para atualizar as areas e o id das pubs
-            const sectorDB = await SectorsModel.findOne({admin:user._id, sector:sectorAcess})
+        //busca todos os setores do banco, para atualizar as areas e o id das pubs
+        const sectorDB = await SectorsModel.findOne({ admin: user._id, sector: sectorAcess })
 
-            let checkAreaExist;
-            let check = sectorDB.areaAndTopics.map((obj)=>{
+        let checkAreaExist;
+        let check = sectorDB.areaAndTopics.map((obj) => {
+            let checkInObj = obj[areaCheck]
+            if (checkInObj) {
+                checkAreaExist = true
+            }
+        })
+
+        //se nao estiver cria a area e dentro dela adiciona os dados
+        //cria um novo elemento para adicionar no banco, incluindo a nova propriedade
+        if (!checkAreaExist === true) {
+            const refAreaAndTopicsEdit = {
+                [areaCheck]: [
+                    refAreaAndTopics
+                ]
+            }
+            const sectorDBAdd = await sectorDB.areaAndTopics.push(refAreaAndTopicsEdit)
+            const updateSector = await SectorsModel.findOneAndUpdate({ admin: user._id, sector: sectorAcess }, sectorDB)
+        }
+
+
+        //caso esteja, apenas adicionar a coleção
+        if (checkAreaExist === true) {
+            const sectorDBAdd = sectorDB.areaAndTopics.map((obj) => {
                 let checkInObj = obj[areaCheck]
-                        if(checkInObj){
-                          checkAreaExist = true
-                        }
+                if (checkInObj) {
+                    let objUpdate = obj[areaCheck].push(refAreaAndTopics)
+
+                    return objUpdate
+                }
+                return obj
             })
 
-            //se nao estiver cria a area e dentro dela adiciona os dados
-            //cria um novo elemento para adicionar no banco, incluindo a nova propriedade
-            if(!checkAreaExist===true){
-                const refAreaAndTopicsEdit = {
-                        [areaCheck]:[
-                            refAreaAndTopics
-                        ]
-                }
-                const sectorDBAdd = await sectorDB.areaAndTopics.push(refAreaAndTopicsEdit)
-                const updateSector = await SectorsModel.findOneAndUpdate({admin:user._id, sector:sectorAcess},sectorDB)
-            }
-
-        
-            //caso esteja, apenas adicionar a coleção
-            if(checkAreaExist===true){
-                const sectorDBAdd = sectorDB.areaAndTopics.map((obj)=>{
-                    let checkInObj = obj[areaCheck]
-                        if(checkInObj){
-                        let objUpdate = obj[areaCheck].push(refAreaAndTopics)
-
-                        return objUpdate
-                        }
-                        return obj
-                })
-
-                const updateSector = await SectorsModel.findOneAndUpdate({admin:user._id, sector:sectorAcess},sectorDB)
-            }
+            const updateSector = await SectorsModel.findOneAndUpdate({ admin: user._id, sector: sectorAcess }, sectorDB)
+        }
 
 
 
-        return res.status(201).json({message:"Tópico criado com sucesso!",data:contentDB})
-        
+        return res.status(201).json({ message: "Tópico criado com sucesso!", data: contentDB })
+
     } catch (error) {
-        return res.status(401).json({message:"Erro ao salvar tópico, tente mais tarde!"}) 
+        return res.status(401).json({ message: "Erro ao salvar tópico, tente mais tarde!" })
     }
 }
 //exibir tópicos de determinado setor para o admin
-exports.getAreas = async (req,res)=>{
+exports.getAreas = async (req, res) => {
     const user = await checkUserForToken(req)
     const id = req.params.id
 
-    if(user.isAdmin!==true){
-        return res.status(401).json({message:"Area somente para administradores!"})
+    if (user.isAdmin !== true) {
+        return res.status(401).json({ message: "Area somente para administradores!" })
     }
 
     try {
-        const contentData = await SectorsModel.find({admin:user._id,_id:id})
-        return res.status(200).json({data:contentData})
-        
+        const contentData = await SectorsModel.find({ admin: user._id, _id: id })
+        return res.status(200).json({ data: contentData })
+
     } catch (error) {
-        return res.status(401).json({message:"Erro ao buscar setores no banco de dados!"})
-    } 
+        return res.status(401).json({ message: "Erro ao buscar setores no banco de dados!" })
+    }
+}
+
+//exclui o tópico, exclui id e titulo do array de areas, dentro do model de setores e excluir o conteudo
+exports.deleteTopic = async (req, res) => {
+
 }
 
 
@@ -126,32 +131,32 @@ exports.getAreas = async (req,res)=>{
 //rotas para os funcionários
 
 //retorna a area e os titulo disponiveis de acordo com o setor do funcionário
-exports.areaAndTitle = async (req,res)=>{
+exports.areaAndTitle = async (req, res) => {
     const user = await checkUserForToken(req)
 
-    try{
-        const areaAndTitle = await SectorsModel.findOne({admin:user.admin,sectorAcess:user.sector}, 'areaAndTopics')
-        return res.status(200).json({data:areaAndTitle})
-    }catch(error){
-        return res.status(401).json({message:"Erro ao buscar os titulos, tente mais tarde"})
+    try {
+        const areaAndTitle = await SectorsModel.findOne({ admin: user.admin, sectorAcess: user.sector }, 'areaAndTopics')
+        return res.status(200).json({ data: areaAndTitle })
+    } catch (error) {
+        return res.status(401).json({ message: "Erro ao buscar os titulos, tente mais tarde" })
     }
 }
 //rota que pega o nome do tópico e filtra os tópicos disponiveis com aquele nome
-exports.getTopics = async (req,res)=>{
+exports.getTopics = async (req, res) => {
     const user = await checkUserForToken(req)
     const idTitle = req.params.id
 
-    if(user.isAdmin){
-        return res.status(200).json({message:"Como você é admin não será possivel especificar o tópico!"})
+    if (user.isAdmin) {
+        return res.status(200).json({ message: "Como você é admin não será possivel especificar o tópico!" })
     }
 
     try {
-        const contentData = await ContentModel.find({admin:user.admin,_id:idTitle})
-        return res.status(200).json({data:contentData})
-        
+        const contentData = await ContentModel.find({ admin: user.admin, _id: idTitle })
+        return res.status(200).json({ data: contentData })
+
     } catch (error) {
-        return res.status(401).json({message:"Erro ao buscar tópicos dessa area, tente mais tarde!"})
+        return res.status(401).json({ message: "Erro ao buscar tópicos dessa area, tente mais tarde!" })
     }
-    
+
 }
 
